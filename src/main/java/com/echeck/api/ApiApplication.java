590faa6079ccbf33409b9e0d093ba25216927759
Affonso Ely <affonsoely@gmail.com>
2025-11-17 22:05:14 -0300

    feat(email): Configuracao SendGrid com nova chave API e remove Security AutoConfig para dev

--- src/main/java/com/echeck/api/ApiApplication.java
+++ src/main/java/com/echeck/api/ApiApplication.java
@@ -2,8 +2,9 @@ package com.echeck.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // <-- Adicione esta exclusão
 public class ApiApplication {
 
 	public static void main(String[] args) {
